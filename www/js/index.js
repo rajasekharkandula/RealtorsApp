312a6853104043a63c70c4b63a659d961eb9d124
Raja <rajasekhar.janani@gmail.com>
2017-09-19 14:56:50 +0530

    Added inAppBrowser

--- www/js/index.js
+++ www/js/index.js
@@ -27,7 +27,7 @@ var app = {
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
-		window.location="http://xucorelms.com/nartesting";
+		window.open('http://xucorelms.com/nartesting', '_system');
         //this.receivedEvent('deviceready');
     },
 	
